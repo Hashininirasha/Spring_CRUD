728fb246ed2eb137c070dcf8b8a39d936090484d
Hashininirasha <hashininirashamunasinghe@gmail.com>
2022-01-01 01:10:50 +0530

    added

---
+++ src/main/java/com/example/student/user/user.java
@@ -0,0 +1,63 @@
+package com.example.student.user;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class user {
+    @Id //map to primary column in table
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 255)
+    private String name;
+
+    @Column(nullable = false, length = 255)
+    private Integer phone;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 255)
+    private String subject;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+}
